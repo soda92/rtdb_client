65de5022bfd35304f2b1e942f15d8743e625d0b7
SodaCris <wang-jiaxun@outlook.com>
2023-09-09 18:19:20 +0800

    add loop in client

--- client.py
+++ client.py
@@ -3,7 +3,10 @@ import psycopg2
 conn = psycopg2.connect(host="localhost", dbname="example", user="soda", password="soda")
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
-cursor = conn.cursor()
-cursor.execute(f"INSERT into realtime_table (key, type, value_float) values (%s, %s, %s)", ("PR_VAL1", "float",  11.2))
-cursor.execute(f"INSERT into realtime_table (key, type, value_str) values (%s, %s, %s)", ("HB_VAL1", "str",  "message"))
-conn.commit()
+for _ in range(1000):
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT into realtime_table (key, type, value_float) values (%s, %s, %s)", ("PR_VAL1", "float",  11.2))
+    cursor.execute(f"INSERT into realtime_table (key, type, value_str) values (%s, %s, %s)", ("HB_VAL1", "str",  "message"))
+    conn.commit()
+    import time
+    time.sleep(0.1)
