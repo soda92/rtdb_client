c696118874f4a4ddf7bc3029ae7d6e7324c55f75
SodaCris <wang-jiaxun@outlook.com>
2023-09-09 17:59:35 +0800

    init

---
+++ listen.py
@@ -0,0 +1,25 @@
+import asyncio
+import psycopg2
+
+# dbname should be the same for the notifying process
+conn = psycopg2.connect(host="localhost", dbname="example", user="soda", password="soda")
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+cursor = conn.cursor()
+cursor.execute(f"LISTEN watch_realtime_table;")
+
+def handle_notify():
+    conn.poll()
+    for notify in conn.notifies:
+        print(notify.payload)
+    conn.notifies.clear()
+
+# It works with uvloop too:
+# import uvloop
+# loop = uvloop.new_event_loop()
+# asyncio.set_event_loop(loop)
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+loop = asyncio.get_event_loop()
+loop.add_reader(conn, handle_notify)
+loop.run_forever()
